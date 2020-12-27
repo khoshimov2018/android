3decba767224b1e819adb8170bbba1b6891c70dd
Gurparsad Singh <thecodingeek@gmail.com>
2020-12-27 20:43:34 +0530

    Added registration activity

--- app/src/main/java/com/example/dating/activities/SplashActivity.kt
+++ app/src/main/java/com/example/dating/activities/SplashActivity.kt
@@ -1,13 +1,30 @@
 package com.example.dating.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.example.dating.R
+import com.example.dating.utils.Constants
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        handle()
+    }
+
+    private fun handle() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            moveToRegister()
+        }, Constants.SPLASH_TIME_OUT)
+    }
+
+    private fun moveToRegister() {
+        val registerActivity = Intent(this, RegisterActivity::class.java)
+        startActivity(registerActivity)
+        finish()
     }
 }
---
+++ app/src/main/java/com/example/dating/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.dating.utils
+
+object Constants {
+    const val SPLASH_TIME_OUT = 3000L
+    const val SHARED_PREF_NAME = "com.example.dating.shared"
+    const val IS_USER_LOGGED_IN = "IS_USER_LOGGED_IN"
+    const val LOGGED_IN_USER = "LOGGED_IN_USER"
+
+    const val LOG_TAG = "DATING_TAG"
+}
