3526b61bd3a4da0c8342fdf42a452d208549421e
Gurparsad Singh <thecodingeek@gmail.com>
2020-12-27 22:48:48 +0530

    Registration & Login UI

--- app/src/main/java/com/example/dating/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/dating/activities/RegisterActivity.kt
@@ -2,11 +2,14 @@ package com.example.dating.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.viewpager2.widget.ViewPager2
 import com.example.dating.R
 import com.example.dating.adapters.RegistrationTabsAdapter
+import com.example.dating.utils.dpToPx
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -36,5 +39,21 @@ class RegisterActivity : AppCompatActivity() {
                 }
             }
         }.attach()
+
+        addKeyboardDetectListener()
+    }
+
+    private fun addKeyboardDetectListener(){
+        val topView = window.decorView.findViewById<View>(android.R.id.content)
+        topView.viewTreeObserver.addOnGlobalLayoutListener {
+            val heightDifference = topView.rootView.height - topView.height
+            if(heightDifference > dpToPx(this, 200F)){
+                // keyboard shown
+                privacyTerms.visibility = View.GONE
+            } else {
+                // keyboard hidden
+                privacyTerms.visibility = View.VISIBLE
+            }
+        }
     }
 }
