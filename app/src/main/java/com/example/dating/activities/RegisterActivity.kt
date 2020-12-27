e5e055091cdaf176390130beb5d7c0f9e98613c3
Gurparsad Singh <thecodingeek@gmail.com>
2020-12-27 21:39:21 +0530

    Added tabs for register

--- app/src/main/java/com/example/dating/activities/RegisterActivity.kt
+++ app/src/main/java/com/example/dating/activities/RegisterActivity.kt
@@ -2,11 +2,39 @@ package com.example.dating.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 import com.example.dating.R
+import com.example.dating.adapters.RegistrationTabsAdapter
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var registrationTabsAdapter: RegistrationTabsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        registrationTabsAdapter = RegistrationTabsAdapter(this)
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+
+        viewPager.adapter = registrationTabsAdapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when(position) {
+                0 -> {
+                    tab.text = getString(R.string.registration)
+                }
+                else -> {
+                    tab.text = getString(R.string.login)
+                }
+            }
+        }.attach()
     }
 }
