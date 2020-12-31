a0279206540904a615119cf9ba4f766e6fd80b99
Gurparsad Singh <thecodingeek@gmail.com>
2021-01-01 00:08:12 +0530

    view model for profiles tab

--- app/src/main/java/com/example/dating/fragments/ProfilesFragment.kt
+++ app/src/main/java/com/example/dating/fragments/ProfilesFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.dating.R
+import com.example.dating.databinding.LoginFragmentBinding
+import com.example.dating.databinding.ProfilesFragmentBinding
 import com.example.dating.viewmodels.ProfilesViewModel
 
 class ProfilesFragment : Fragment() {
@@ -16,18 +19,31 @@ class ProfilesFragment : Fragment() {
     }
 
     private lateinit var viewModel: ProfilesViewModel
+    private lateinit var binding: ProfilesFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProfilesViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.profiles_fragment, container, false)
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.profiles_fragment, container, false)
+        val view: View = binding.root
+        initViewModel()
+
+        return view
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfilesViewModel::class.java)
-        // TODO: Use the ViewModel
+    private fun initViewModel() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        initObservers()
     }
 
+    private fun initObservers() {
+
+    }
 }
