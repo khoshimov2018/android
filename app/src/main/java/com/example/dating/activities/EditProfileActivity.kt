da27606e727f67e7fe0cd65c3cf6f53ea10895b9
Gurparsad Singh <thecodingeek@gmail.com>
2021-01-01 00:02:39 +0530

    Edit Profile ViewModel

--- app/src/main/java/com/example/dating/activities/EditProfileActivity.kt
+++ app/src/main/java/com/example/dating/activities/EditProfileActivity.kt
@@ -2,10 +2,37 @@ package com.example.dating.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.dating.R
+import com.example.dating.databinding.ActivityEditProfileBinding
+import com.example.dating.viewmodels.EditProfileViewModel
 
 class EditProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditProfileBinding
+    private lateinit var editProfileViewModel: EditProfileViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_profile)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_edit_profile)
+        binding.lifecycleOwner = this
+        initViewModel()
+    }
+
+    private fun initViewModel() {
+        editProfileViewModel = ViewModelProvider(this).get(EditProfileViewModel::class.java)
+        binding.viewModel = editProfileViewModel
+
+        initObservers()
+    }
+
+    private fun initObservers() {
+        editProfileViewModel.getBackButtonClicked().observe(this, Observer { isPressed: Boolean ->
+            if (isPressed) {
+                this.onBackPressed()
+            }
+        })
     }
 }
