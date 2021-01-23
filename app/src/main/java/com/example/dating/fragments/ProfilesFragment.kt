51734d6cbe0151b7bcf76f134ed07deb67af44bc
Gurparsad Singh <thecodingeek@gmail.com>
2021-01-23 19:13:54 +0530

    Get Location

--- app/src/main/java/com/example/dating/fragments/ProfilesFragment.kt
+++ app/src/main/java/com/example/dating/fragments/ProfilesFragment.kt
@@ -1,11 +1,26 @@
 package com.example.dating.fragments
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
+import android.net.Uri
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -13,23 +28,26 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.dating.R
 import com.example.dating.databinding.ProfilesFragmentBinding
 import com.example.dating.models.UserModel
-import com.example.dating.utils.getLoggedInUserFromShared
-import com.example.dating.utils.showInfoAlertDialog
-import com.example.dating.utils.validateResponse
+import com.example.dating.utils.*
 import com.example.dating.viewmodels.ProfilesViewModel
-
-private const val NUM_PAGES = 5
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 
 class ProfilesFragment : Fragment() {
 
     companion object {
         fun newInstance() = ProfilesFragment()
+        private const val LOCATION_PERMISSION = 201
     }
 
     private lateinit var viewModel: ProfilesViewModel
     private lateinit var binding: ProfilesFragmentBinding
 
     private lateinit var viewPager: ViewPager2
+    private var comingFromSettings = false
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -55,25 +73,176 @@ class ProfilesFragment : Fragment() {
         initObservers()
 
         viewModel.setLoggedInUser(getLoggedInUserFromShared(requireActivity()))
-        viewModel.getFilters()
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(
+                    requireActivity(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                fetchLocation()
+            } else {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        requireActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    )
+                ) {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        LOCATION_PERMISSION
+                    )
+                } else {
+                    requestPermissions(
+                        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                        LOCATION_PERMISSION
+                    )
+                }
+            }
+        } else {
+            fetchLocation()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            LOCATION_PERMISSION -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    fetchLocation()
+                } else {
+                    showLocationPopup()
+                }
+            }
+        }
+    }
+
+    private fun showLocationPopup() {
+        val alertBuilder = AlertDialog.Builder(requireActivity())
+        alertBuilder.setCancelable(false)
+        alertBuilder.setMessage(getString(R.string.need_location_permission))
+
+        alertBuilder.setPositiveButton(
+            getString(R.string.ok),
+            DialogInterface.OnClickListener { dialogInterface, _ ->
+                dialogInterface.cancel()
+                openSettings()
+            })
+
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun openSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", requireActivity().packageName, null)
+        intent.data = uri
+        comingFromSettings = true
+        startActivity(intent)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (comingFromSettings) {
+            comingFromSettings = false
+            checkLocationPermission()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchLocation() {
+        val locationRequest = LocationRequest.create()?.apply {
+            interval = 10000
+            fastestInterval = 5000
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+        val builder =
+            locationRequest?.let { LocationSettingsRequest.Builder().addLocationRequest(it) }
+
+        val client: SettingsClient = LocationServices.getSettingsClient(requireActivity())
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder?.build())
+
+        task.addOnSuccessListener { _ ->
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+            locationCallback = object : LocationCallback() {
+                override fun onLocationResult(locationResult: LocationResult?) {
+                    locationResult ?: return
+                    for (location in locationResult.locations){
+                        viewModel.getFilters()
+                        fusedLocationClient.removeLocationUpdates(locationCallback)
+                        break
+                    }
+                }
+            }
+
+            fusedLocationClient.requestLocationUpdates(locationRequest,
+                locationCallback,
+                Looper.getMainLooper())
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                try {
+                    val resolvable: ResolvableApiException = exception
+                    startIntentSenderForResult(resolvable.resolution.intentSender, LocationRequest.PRIORITY_HIGH_ACCURACY, null, 0, 0, 0, null);
+                } catch (e: IntentSender.SendIntentException) {
+                    couldNotGetLocationPopup()
+                }
+            } else {
+                couldNotGetLocationPopup()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            LocationRequest.PRIORITY_HIGH_ACCURACY -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    fetchLocation()
+                } else {
+                    couldNotGetLocationPopup()
+                }
+            }
+        }
+    }
+
+    private fun couldNotGetLocationPopup() {
+        val alertBuilder = AlertDialog.Builder(requireActivity())
+        alertBuilder.setCancelable(false)
+        alertBuilder.setMessage(getString(R.string.could_not_get_location))
+
+        alertBuilder.setPositiveButton(getString(R.string.ok)) { dialogInterface, _ ->
+            dialogInterface.cancel()
+            requireActivity().finishAffinity()
+        }
+
+        val alertDialog = alertBuilder.create()
+        alertDialog.show()
     }
 
     private fun initObservers() {
         viewModel.getFilterModelLiveData().observe(viewLifecycleOwner, {
-            if(it != null) {
+            if (it != null) {
                 viewModel.getUsers()
             }
         })
 
         viewModel.getUsersListLiveData().observe(viewLifecycleOwner, {
-            if(it != null) {
+            if (it != null) {
                 val pagerAdapter = ScreenSlidePagerAdapter(requireActivity(), it)
                 viewPager.adapter = pagerAdapter
             }
         })
 
         viewModel.getShowNoInternet().observe(viewLifecycleOwner, {
-            if(it) {
+            if (it) {
                 viewModel.setShowNoInternet(false)
                 showInfoAlertDialog(requireActivity(), getString(R.string.no_internet))
             }
@@ -87,7 +256,10 @@ class ProfilesFragment : Fragment() {
         })
     }
 
-    private inner class ScreenSlidePagerAdapter(fa: FragmentActivity, val usersList: MutableList<UserModel>) : FragmentStateAdapter(fa) {
+    private inner class ScreenSlidePagerAdapter(
+        fa: FragmentActivity,
+        val usersList: MutableList<UserModel>
+    ) : FragmentStateAdapter(fa) {
         override fun getItemCount(): Int = usersList.size
 
         override fun createFragment(position: Int): Fragment {
