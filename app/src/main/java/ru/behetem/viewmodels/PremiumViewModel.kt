115f8ec1665145d39859e03f2e5f1defd3dab81c
Gurparsad Singh <thecodingeek@gmail.com>
2021-03-28 13:18:48 +0530

    UI changes for Premium

--- app/src/main/java/ru/behetem/viewmodels/PremiumViewModel.kt
+++ app/src/main/java/ru/behetem/viewmodels/PremiumViewModel.kt
@@ -1,4 +1,10 @@
 package ru.behetem.viewmodels
 
+import android.view.View
+
 class PremiumViewModel: BaseViewModel() {
+
+    fun onCancelled(view: View) {
+        setBackButtonClicked(true)
+    }
 }
