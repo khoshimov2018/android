f5810500553d5f0948c7345aab930a9e67827045
Gurparsad Singh <thecodingeek@gmail.com>
2021-03-31 08:45:12 +0530

    fetch subscriptions

--- app/src/main/java/ru/behetem/activities/PremiumActivity.kt
+++ app/src/main/java/ru/behetem/activities/PremiumActivity.kt
@@ -5,20 +5,26 @@ import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.android.billingclient.api.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import ru.behetem.R
 import ru.behetem.databinding.ActivityPremiumBinding
+import ru.behetem.utils.printLog
 import ru.behetem.viewmodels.PremiumViewModel
 
 class PremiumActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPremiumBinding
     private lateinit var premiumViewModel: PremiumViewModel
+    private lateinit var billingClient: BillingClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_premium)
         binding.lifecycleOwner = this
         initViewModel()
+        initBilling()
     }
 
     private fun initViewModel() {
@@ -34,5 +40,44 @@ class PremiumActivity : AppCompatActivity() {
                 this.onBackPressed()
             }
         })
+    }
+
+    private fun initBilling() {
+        val purchasesUpdatedListener = PurchasesUpdatedListener { billingResult, purchases ->
+            // To be implemented in a later section.
+        }
+
+        billingClient = BillingClient.newBuilder(this)
+            .setListener(purchasesUpdatedListener)
+            .enablePendingPurchases()
+            .build()
+
+        billingClient.startConnection(object : BillingClientStateListener {
+            override fun onBillingSetupFinished(billingResult: BillingResult) {
+                if (billingResult.responseCode == BillingClient.BillingResponseCode.OK) {
+                    // The BillingClient is ready. You can query purchases here.
+                    querySkuDetails()
+                }
+            }
+
+            override fun onBillingServiceDisconnected() {
+                // Try to restart the connection on the next request to
+                // Google Play by calling the startConnection() method.
+            }
+        })
+    }
+
+    fun querySkuDetails() {
+        val skuList = ArrayList<String>()
+        skuList.add("1_month")
+        skuList.add("3_months")
+        skuList.add("6_months")
+        val params = SkuDetailsParams.newBuilder()
+        params.setSkusList(skuList).setType(BillingClient.SkuType.SUBS)
+        billingClient.querySkuDetailsAsync(params.build()) { billingResult, skuDetailsList ->
+            // Process the result.
+            printLog("details $skuDetailsList")
+            printLog("billingResult ${billingResult.responseCode} ${billingResult.debugMessage}")
+        }
     }
 }
